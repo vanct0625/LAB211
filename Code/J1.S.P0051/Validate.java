791bc08ea431cb8991bb47a8d1616026e1176050
Chu Thế Văn <91040935+vanct0625@users.noreply.github.com>
2024-03-24 00:08:30 +0700

    Update Validate.java

--- Code/J1.S.P0051/Validate.java
+++ Code/J1.S.P0051/Validate.java
@@ -1,16 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package calculator;
 
 import java.util.Scanner;
 
-/**
- *
- * @author chuth
- */
 public class Validate {
     
     private static final Scanner in = new Scanner(System.in);
